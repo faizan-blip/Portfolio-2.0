46d917febcb867aee64f350f2677b4aace70357b
IEEE-VSSUT-SB <ieeevssutburlasb@gmail.com>
2023-01-30 21:11:33 +0530

    changes

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     // });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front End Web Devloper"],
+        strings: ["Front End Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
